0ea8f17c8941a35b0177961f2ae1bd29243ee148
Luke Campbell <LukeCampbell5853@gmail.com>
2023-11-18 09:52:48 +1100

    Update test.py

--- motor/test.py
+++ motor/test.py
@@ -1,11 +1,12 @@
 from motor_lib import motor
+from time import sleep
 
 motor1 = motor(0,1)
             
 while True:
     for speed in range(-100,100,1):
-        motor1.set_speed(speed)
+        motor1.speed(speed)
         sleep(0.01)
     for speed in range(100,-100,-1):
-        motor1.set_speed(speed)
+        motor1.speed(speed)
         sleep(0.01)
