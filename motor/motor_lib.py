5711b2b39dddf41111231db77bd51633de9fb31d
Luke Campbell <LukeCampbell5853@gmail.com>
2023-11-18 09:49:30 +1100

    Update motor_lib.py

--- motor/motor_lib.py
+++ motor/motor_lib.py
@@ -1,17 +1,20 @@
 from machine import Pin, PWM
-from utime import sleep
 
 class motor:
-    def __init__(self,pin1,pin2):
-        self.__pin1 = PWM(Pin(pin1))
-        self.__pin2 = PWM(Pin(pin2))
+    def __init__(self,p1,p2):
+        self.__p1 = PWM(Pin(p1))
+        self.__p2 = PWM(Pin(p2))
+        self.__p1.freq(1000)
+        self.__p2.freq(1000)
+        self.__p1.duty_u16(0)
+        self.__p2.duty_u16(0)
         self.__signal = 0
     
-    def set_speed(self,speed):
-        self.__signal = int((speed/100)*65025)
+    def speed(self,speed):
+        self.__signal = abs(int((speed/100)*65025))
         if speed > 0:
-            self.__pin1.duty_u16(0)
-            self.__pin2.duty_u16(self.__signal)
+            self.__p1.duty_u16(0)
+            self.__p2.duty_u16(self.__signal)
         else:
-            self.__pin2.duty_u16(0)
-            self.__pin1.duty_u16(self.__signal)
+            self.__p2.duty_u16(0)
+            self.__p1.duty_u16(self.__signal)
