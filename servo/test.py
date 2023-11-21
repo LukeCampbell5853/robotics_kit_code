7ef127a7523a1fd91a8ada1bc4e5cdd8362c6c23
Luke Campbell <LukeCampbell5853@gmail.com>
2023-11-22 09:12:22 +1100

    Update test.py

--- servo/test.py
+++ servo/test.py
@@ -1,9 +1,9 @@
 import servo_lib        
 
-servo1 = servo_lib.servo(0)
+servo = servo_lib.servo(0)
 
 while True:
-    servo1.goto(180)
-    servo1.wait()
-    servo1.goto(0)
-    servo1.wait()
+    servo.goto(180)
+    servo.wait()
+    servo.goto(0)
+    servo.wait()
